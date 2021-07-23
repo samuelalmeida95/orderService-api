3aec5fd890bfdc874411994c8adec41b8b9b4dd4
samuelalmeida95 <samuka.ab.melo@hotmail.com>
2021-07-23 14:10:41 -0300

    base de dados automatica

--- src/main/java/com/samuel/crudos/services/DBservice.java
+++ src/main/java/com/samuel/crudos/services/DBservice.java
@@ -25,48 +25,27 @@ public class DBservice {
   private OSRepository osRepository;
 
   public void instanciaDB() {
-    Tecnico t1 = new Tecnico(
-      null,
-      "Samuel Melo",
-      "144.785.300-84",
-      "(83) 99617-2418"
-    );
+    Tecnico t1 = new Tecnico(null, "Samuel Melo", "144.785.300-84", "(83) 99617-2418");
 
-    Tecnico t2 = new Tecnico(
-      null,
-      "Matias Melo",
-      "144.785.300-84",
-      "(83) 99617-2418"
-    );
+    Tecnico t2 = new Tecnico(null, "Matias Melo", "144.785.300-84", "(83) 99617-2418");
 
-    Tecnico t3 = new Tecnico(
-      null,
-      "Lilian Melo",
-      "144.785.300-84",
-      "(83) 99617-2418"
-    );
+    Tecnico t3 = new Tecnico(null, "Lilian Melo", "144.785.300-84", "(83) 99617-2418");
 
-    Cliente c1 = new Cliente(
-      null,
-      "Mussun",
-      "598.508.200-80",
-      "(88) 98888-7777"
-    );
+    Cliente c1 = new Cliente(null, "Mussun", "598.508.200-80", "(88) 98888-7777");
 
-    OS os1 = new OS(
-      null,
-      Prioridade.ALTA,
-      Status.ANDAMENTO,
-      "Teste DB",
-      t1,
-      c1
-    );
+    OS os1 = new OS(null, Prioridade.ALTA, Status.ANDAMENTO, "Acesso ao JIRA", t1, c1);
+
+    OS os2 = new OS(null, Prioridade.BAIXA, Status.ABERTO, "Tarefa com impedimento", t3, c1);
+
+    OS os3 = new OS(null, Prioridade.MEDIA, Status.ENCERRADO, "Migrar IDE para VsCode", t2, c1);
 
     t1.getOrdensServico().add(os1);
+    t2.getOrdensServico().add(os2);
+    t3.getOrdensServico().add(os3);
     c1.getOrdensServico().add(os1);
 
     tecnicoRepository.saveAll(Arrays.asList(t1, t2, t3));
     clienteRepository.saveAll(Arrays.asList(c1));
-    osRepository.saveAll(Arrays.asList(os1));
+    osRepository.saveAll(Arrays.asList(os1, os2, os3));
   }
 }
